85f054ab45a344971ef3af5248ff2eff32899a2d
sjj <395414080@qq.com>
2019-05-21 11:20:44 +0800

    first

---
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -0,0 +1,10 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by sjj1127 on 2019-5-21.
+ */
+@Controller
+public class UserController {
+}
